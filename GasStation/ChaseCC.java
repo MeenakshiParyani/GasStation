653dfd3f29f3c374cc8677621460b88f6ba5733f
MeenakshiParyani <meenakshi.paryani@sjsu.edu>
2017-04-07 11:43:12 -0700

    Initial Class Setup

---
+++ GasStation/ChaseCC.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ChaseCC here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ChaseCC extends Card
+{
+    /**
+     * Act - do whatever the ChaseCC wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ GasStation/DiscoverCC.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class DiscoverCC here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DiscoverCC extends Card
+{
+    /**
+     * Act - do whatever the DiscoverCC wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
