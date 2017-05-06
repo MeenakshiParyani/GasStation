4f5463d99c01757810d38c8af7866270a74c9f59
Rimpy Bharot <rbverses21@gmail.com>
2017-05-05 20:42:41 -0700

    update calling logc

--- GasStation/PumpNozel.java
+++ GasStation/PumpNozel.java
@@ -12,46 +12,75 @@ public class PumpNozel extends Screen implements IDisplayComponent
 {	
 	private World world;
 	Clock c = new Clock();
-	public boolean draw = false;
+	public boolean draw = true;
+	public boolean dispenseStart = false;
+	public boolean receiptCalled = false;
+	public int fuelType = 87;
+	public boolean carWash = false;
 
-	
+
 	public PumpNozel(World world) {
 		this.world = world;
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-     * Act - do whatever the PumpNozel wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-    	
-    	int mouseX, mouseY;
-    	   
-     
-        if(Greenfoot.mouseDragged(this))  
-        {
-        	//System.out.println("dragging");
-            MouseInfo mouse = Greenfoot.getMouseInfo();
-            mouseX=mouse.getX();
-            mouseY=mouse.getY();
-            setLocation(mouseX, mouseY);
-        }
-    	Actor pumpNozel = getOneIntersectingObject(Car.class);
-    	if(pumpNozel!=null && draw==true){
-    		c.drawGas();
-    	}
-        
-        this.getWorld().addObject(c, 448, 168);
-        
-        
-        System.out.println(new DecimalFormat(("00.00")).format(c.gallons));
 
-    }
-    
+	public void act() 
+	{
+
+		int mouseX, mouseY;
+
+
+		if(Greenfoot.mouseDragged(this))  
+		{
+			//System.out.println("dragging");
+			MouseInfo mouse = Greenfoot.getMouseInfo();
+			mouseX=mouse.getX();
+			mouseY=mouse.getY();
+			setLocation(mouseX, mouseY);
+		}
+		Actor pumpNozel = getOneIntersectingObject(Car.class);
+		Actor nozzleHolder  = getOneIntersectingObject(NozzleHolder.class);
+
+
+		if(draw==true && pumpNozel!=null){
+			c.drawGas();
+			c.fuellingStart = true;
+
+		}
+		if(c.fuellingStart && nozzleHolder!=null){
+			c.fuellingDone = true;
+		}
+
+		if(c.fuellingStart && c.fuellingDone && !receiptCalled){
+			draw = false;
+			carWash();
+			callReceipt();
+		}
+		this.getWorld().addObject(c, 448, 168);
+	}
+
+	private void carWash() {
+		
+		
+
+		
+	}
+
+
+
 	public void display() {
 		world.addObject((Actor) this, 800, 150);
 	}
- 
+
+
+	public void callReceipt(){
+		System.out.println("called");
+		c.fuellingDone = c.fuellingDone = false;
+		receiptCalled = true;
+		
+		
+		
+}
+
 }
